60c9d91552c841feeae530155de03824f859aafb
analytics-bootcamp <olavlaudy@ph.ibm.com>
2017-02-17 07:54:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 #import addutils
 
 setup(name='AddUtils',
-      version=addutils.__version__,
+      version=1.1,
       description='Addfor tutorials visualization utilities',
       author='Daniele Trainini',
       author_email='daniele.trainini@add-for.com',
